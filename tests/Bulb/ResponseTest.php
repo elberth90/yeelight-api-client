892406d99789622292f0e6450d5b7608428b15c4
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2017-01-23 01:06:05 +0100

    Add unit tests for Response

---
+++ tests/Bulb/ResponseTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Bulb;
+
+use Yeelight\Bulb\Exceptions\BulbCommandException;
+use Yeelight\Bulb\Response;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_success_Response()
+    {
+        $response = new Response(
+            [
+                'id' => 1,
+                'result' => ['foo']
+            ]
+        );
+
+        $this->assertTrue($response->isSuccess());
+        $this->assertEquals(1, $response->getDeviceId());
+        $this->assertEquals(['foo'], $response->getResult());
+    }
+
+    public function test_error_Response()
+    {
+        $response = new Response(
+            [
+                'id' => 1,
+                'error' => [
+                    'code' => 500,
+                    'message' => 'error'
+                ]
+            ]
+        );
+
+        $this->assertFalse($response->isSuccess());
+        $this->assertEquals(1, $response->getDeviceId());
+        $this->assertEmpty($response->getResult());
+        $this->assertInstanceOf(BulbCommandException::class, $response->getException());
+    }
+}
